61d073adb4bd927cf124c8b4fd8e4f0585fb941a
PRIYANSHU SINGH <235023819+Kizaru-dev@users.noreply.github.com>
2026-09-06 14:59:30 +0530

    first commit

---
+++ src/main/java/com/priyanshu/student_management_system_3/StudentManagementSystem3Application.java
@@ -0,0 +1,13 @@
+package com.priyanshu.student_management_system_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystem3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystem3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/priyanshu/student_management_system_3/StudentManagementSystem3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyanshu.student_management_system_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystem3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
